3fb71bf5409c4e44a8451182a496946c31013151
sophy-munala <101244562+sophy-munala@users.noreply.github.com>
2022-06-14 14:40:54 +0300

    Add files via upload

---
+++ decrypt.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<title>Testing 123..</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+	 <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;600&display=swap" rel="stylesheet">
+    <style type="text/css">
+    	
+    	
+    </style>
+</head>
+<body>
+	<form action="">
+		<div class="title">
+			<h2>CRYPTOPAL</h2>
+			<h2>Decrypt your Messages Securely with Cryptopal</h2>
+		</div>
+		<div class="half">
+			<div class="item">
+				<label for="key"><h3>Key:</h3></label>
+				<input type="text" name="" id="key">
+			</div>
+			
+		</div>
+		<div class="full">
+			<label for="msg"><h3>The Cipher Text:</h3></label>
+			<textarea name="" id="msg"></textarea>
+		</div>
+		<div class="action">
+			<a target="_blank" href="decrypted.php" class="submit-btn">Decrypt Message</a>
+		</div>
+		<div class="icons">
+			<a href="" class="fa fa-twitter"></a>
+			<a href="" class="fa fa-facebook"></a>
+			<a href="" class="fa fa-instagram"></a>
+			<a href="" class="fa fa-youtube"></a>
+		</div>
+	</form>
+</body>
+</html>
